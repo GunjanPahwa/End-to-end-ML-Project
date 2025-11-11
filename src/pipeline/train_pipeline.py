7db59e02c31519a3b12a4f7f74cbdcbe3bdb9846
GunjanPahwa <gunjan19pahwa@gmail.com>
2025-11-11 23:43:25 +0530

    Logging and Exception

---
+++ src/pipeline/train_pipeline.py
@@ -0,0 +1,2 @@
+#i will try to  call all component files from here
+#A pipeline in ML is a tool (conceptually or programmatically) that chains together multiple data processing and modeling steps so that the entire ML process can be executed as a single unit.
